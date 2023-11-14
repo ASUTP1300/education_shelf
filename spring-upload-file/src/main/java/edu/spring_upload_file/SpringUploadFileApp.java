a97ee709ea1d0aa0d262badfe749ae616f6c8b00
rmanasipov@beeline.ru <rmanasipov@beeline.ru>
2023-11-14 10:06:35 +0300

    SPRING_UPLOADFILE: Редактировал контроллер

--- spring-upload-file/src/main/java/edu/spring_upload_file/SpringUploadFileApp.java
+++ spring-upload-file/src/main/java/edu/spring_upload_file/SpringUploadFileApp.java
@@ -1,10 +1,13 @@
 package edu.spring_upload_file;
 
+import edu.spring_upload_file.client.UploadClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
-@EnableFeignClients
+@EnableFeignClients(
+        clients = UploadClient.class
+)
 public class SpringUploadFileApp {
     public static void main(String[] args) {
         SpringApplication.run(SpringUploadFileApp.class, args);
--- spring-upload-file/src/main/java/edu/spring_upload_file/client/UploadClient.java
+++ spring-upload-file/src/main/java/edu/spring_upload_file/client/UploadClient.java
@@ -1,21 +1,24 @@
-//package edu.spring_upload_file.client;
-//
-//import edu.spring_upload_file.config.FeignSupportConfig;
-//import org.springframework.cloud.openfeign.FeignClient;
-//import org.springframework.http.MediaType;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestPart;
-//import org.springframework.web.multipart.MultipartFile;
-//
-//
-//@FeignClient(
-//        name = "file",
-//        url = "http://localhost:9066",
-//        configuration = FeignSupportConfig.class
-//)
-//public interface UploadClient {
-//
-//    @PostMapping(value = "/upload-file", consumes = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-//    void fileUpload(@RequestPart(value = "file") MultipartFile file);
-//}
+package edu.spring_upload_file.client;
+
+import edu.spring_upload_file.config.FeignSupportConfig;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@FeignClient(
+        name = "file",
+        url = "http://localhost:9066",
+        configuration = FeignSupportConfig.class
+)
+public interface UploadClient {
+
+    @PostMapping(value = "/upload")
+    public ResponseEntity<String> fileUpload(
+            @RequestParam(name = "files") MultipartFile[] files,
+            @RequestParam(name = "bank") String bank,
+            @RequestParam(name = "pattern")  String pattern
+    );
+}
